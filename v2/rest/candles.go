b68b07366955d7a8ccee502eaa620e1bb1b4b3ed
Danny <dannyluong408@gmail.com>
2018-07-05 20:10:20 -0700

    Update

--- v2/rest/candles.go
+++ v2/rest/candles.go
@@ -34,8 +34,8 @@ func (p *CandleService) GetOHLCV(timeframe string, symbol string, start int64, e
   res = make([]*bitfinex.Candle, num)
 
   for i := 0; i < num; i++ {
-			fields := strings.Fields(fmt.Sprintf("%v", data[i]))
-			fields := fields[1:len(fields)-1]
+			raw := strings.Fields(fmt.Sprintf("%v", data[i]))
+			fields := raw[1:len(raw)-1]
 			for _, res := range fields{
 				fmt.Println(res)
 			}
