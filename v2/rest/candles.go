12019385ab189a1368b6ef469e0ce5a1b5005988
Danny <dannyluong408@gmail.com>
2018-07-05 19:24:17 -0700

    Update

--- v2/rest/candles.go
+++ v2/rest/candles.go
@@ -2,9 +2,7 @@ package rest
 
 import (
 	"fmt"
-	"reflect"
-	"strings"
-	bitfinex "github.com/dannyluong408/bitfinex-api-go/v2"
+	"github.com/dannyluong408/bitfinex-api-go/v2"
 )
 
 type CandleService struct {
@@ -21,40 +19,14 @@ func (p *CandleService) GetOHLCV(timeframe string, symbol string, start int64, e
 		return []*Candle{}, err
 	}
 
-  // fmt.Println("1:")
-	// fmt.Println(reflect.TypeOf(data))
-	// fmt.Println("2:")
-	// fmt.Println(data)
-	// fmt.Println("3:")
-	// fmt.Println(len(data))
-	fmt.Println("4:")
-	fmt.Println(reflect.TypeOf(data[0]))
-	fmt.Println("5:")
-	fmt.Println(data[0])
-
+	resolution := CandleResolutionFromString(timeframe)
   num := len(data)
   res = make([]*Candle, num)
 
-  for i := 0; i < num; i++ {
-		words := strings.Fields(rawdata[i])
-    res[i] = &Candle{
-			Timestamp:                bitfinex.i64ValOrZero(words[0]),
-			Open:                     bitfinex.sValOrEmpty(words[1]),
-			High:                     bitfinex.sValOrEmpty(words[2]),
-			Low:                      bitfinex.sValOrEmpty(words[3]),
-			Close:                    bitfinex.sValOrEmpty(words[4]),
-			Volume:                   bitfinex.sValOrEmpty(words[5]),
-		}
+  for i, candle := range data {
+			output, err = NewCandleSnapshotFromRaw(symbol, resolution, rawdata)
+			res[i] := output
   }
+	
 	return res, nil
-}
-
-// Kline define kline info
-type Candle struct {
-	Timestamp                int64  `json:"openTime"`
-	Open                     string `json:"open"`
-	High                     string `json:"high"`
-	Low                      string `json:"low"`
-	Close                    string `json:"close"`
-	Volume                   string `json:"volume"`
 }
