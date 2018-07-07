94df22ee61febd655f5350459b2c8a46c28cee0b
Danny <dannyluong408@gmail.com>
2018-07-07 12:44:06 -0700

    update

--- v2/rest/candles.go
+++ v2/rest/candles.go
@@ -12,7 +12,7 @@ type CandleService struct {
 // Gets KLine History using v2 api
 func (p *CandleService) GetOHLCV(timeframe string, symbol string, start int64, end int64) (res []*bitfinex.Candle, err error) {
   endpoint := "candles/trade:" + timeframe + ":" + symbol + "/hist?start=" + string(start) + "&end=" + string(end)
-
+	
 	raw, err := p.Request(NewRequestWithMethod(endpoint, "GET"))
 
 	if err != nil {
